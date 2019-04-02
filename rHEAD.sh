d8e1be1e006c6c62ccf3fecebccd346f3c6dc9d2
Josiah Schwab <jschwab@gmail.com>
2019-04-02 11:08:30 -0700

    Add batch scripts for runs

---
+++ rHEAD.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+#PBS -N 1M_pre_ms_to_wd
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=24:00:00
+#PBS -V
+#PBS -m abe
+#PBS -M jwschwab@ucsc.edu
+
+if [ -n "${PBS_O_WORKDIR}" ]; then
+   cd ${PBS_O_WORKDIR}
+   module load mesasdk/20190315
+   export MESA_DIR=/pfs/jschwab/mesa-svn-test
+   export OMP_NUM_THREADS=16
+fi
+
+# rebuild MESA
+./clean
+./mk
+
+# link 
+ln -sf inlist_rHEAD inlist_case
+
+# run MESA
+./rn
+
